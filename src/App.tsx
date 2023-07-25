8ee87e86749dc288d57e10a405ddbc598faf7ea5
Denis Klintukhov <denis.klintukhov@gmail.com>
2023-07-25 16:51:49 +0300

    add: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+
 import { ErrorBoundary } from 'react-error-boundary';
 import { ErrorBoundaryFallback } from './components';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { HomePage, InfoPage } from './pages';
 import './App.css';
 
 function App() {
   return (
     <ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/info' element={<InfoPage />} />
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
     </ErrorBoundary>
   );
 }
